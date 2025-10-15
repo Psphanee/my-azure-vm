ec3f9fa3d07f119b496bd8ad1a613754d49a0223
phaneeps <phaneeps@gmail.com>
2025-10-15 11:32:31 +0530

    updated

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "myPublicIP"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static" # changed from Dynamic to Static
   sku                 = "Standard"
 }
 
