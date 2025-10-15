ec11c965ef573bf663a5dccae318725ee50c8a43
phaneeps <phaneeps@gmail.com>
2025-10-15 10:05:51 +0530

    new code

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  default     = "rg-terraform-vm"
+}
+
+variable "location" {
+  description = "Azure region"
+  default     = "East US"
+}
